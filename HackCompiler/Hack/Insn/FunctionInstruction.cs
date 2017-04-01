ac9b0ee882c7f72d596c2966db857182d07d6912
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2017-03-31 22:18:47 -0400

    Successfully compiling Function and Return now. I only have to tackle 'call' and bootstrapping now.

---
+++ HackCompiler/Hack/Insn/FunctionInstruction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackCompiler.Hack.Insn
+{
+    public class FunctionInstruction : Instruction
+    {
+        private string AssemblyPushZero = @"@SP
+A=M
+M=0
+@SP
+M=M+1
+";
+        // Right now, m_ClassName isn't used, but I have
+        // it incase in the future I need to use it for 
+        // naming clashes.
+        private string m_ClassName;
+        private string m_FunctionName;
+        private int m_NumArgs;
+
+        public FunctionInstruction(string className, string functionName, int numArgs)
+        {
+            m_ClassName = className;
+            m_FunctionName = functionName;
+            m_NumArgs = numArgs;
+        }
+
+        public override string GenerateAssembly()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(string.Format("({0})\n", m_FunctionName));
+
+            // Reserve space for the arguments.
+            for(int i = 0; i < m_NumArgs; i++)
+            {
+                builder.Append(AssemblyPushZero);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ HackCompiler/Hack/Insn/ReturnInstruction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackCompiler.Hack.Insn
+{
+    public class ReturnInstruction : Instruction
+    {
+        private const string FrameOffsetFormat = @"@{1}
+D=D-A
+A=D
+D=M
+@{0}
+M=D
+";
+
+        private const string ReturnFormat = @"@LCL
+D=M
+@frame
+M=D
+{0}
+{1}
+@ARG
+A=M
+M=D
+@ARG
+D=M+1
+@SP
+M=D
+{2}
+{3}
+{4}
+{5}
+@retAddr
+A=M
+0;JMP
+";
+
+        private static string GenerateFrameOffset(string location, int offset, bool loadFrame = true)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if(loadFrame)
+            {
+                builder.Append(@"@frame
+D=M
+");
+            }
+
+            builder.AppendFormat(FrameOffsetFormat, location, offset);
+
+            return builder.ToString();
+        }
+
+        public override string GenerateAssembly()
+        {
+            return String.Format(ReturnFormat,
+                GenerateFrameOffset("retAddr", 5, false),
+                GeneratePopAssembly(),
+                GenerateFrameOffset("THAT", 1),
+                GenerateFrameOffset("THIS", 2),
+                GenerateFrameOffset("ARG", 3),
+                GenerateFrameOffset("LCL", 4)
+                );
+        }
+    }
+}
